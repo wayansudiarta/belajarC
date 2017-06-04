6a7210c0e431f59b8a28c66b23911484ffca1756
wayansudiarta <wayan.sudiarta@gmail.com>
2017-06-04 20:38:02 +0800

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,14 @@
+/* Program hello.c
+   Menampilkan text " Hello World !!!"
+*/
+
+#include<stdio.h>
+
+
+int main()
+{
+   // tampilkan pada layar
+   printf(" Hello World !!!\n");
+
+   return 0;
+}
